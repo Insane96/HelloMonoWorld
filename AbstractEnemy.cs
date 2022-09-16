27aa77270c5382b0ad619b41ce00b43bc917aec9
Insane96 <delvillano.alberto@gmail.com>
2022-09-16 15:29:11 +0200

    Enemy now attacks when reaches the player

--- AbstractEnemy.cs
+++ AbstractEnemy.cs
@@ -5,18 +5,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloMonoWorld.Engine;
 
 namespace HelloMonoWorld;
 
 public class AbstractEnemy : Entity
 {
+
+    public double attackSpeed = 1d;
+    public double attackTime;
+    public double attackDamage = 1d;
+
     public AbstractEnemy(string id, string spriteName) : base(id, spriteName)
     {
-
+        this.attackTime = this.attackSpeed;
     }
 
     public override void Update()
     {
+        if (this.position.X <= 300)
+        {
+            this.deltaMovement = Vector2.Zero;
+            if (this.attackTime > 0d)
+            {
+                this.attackTime -= Time.DeltaTime;
+                if (this.attackTime <= 0d)
+                {
+                    MainGame.player.Hurt(this.attackDamage, 0d);
+                    this.attackTime = this.attackSpeed;
+                }
+            }
+        }
         base.Update();
     }
 }
