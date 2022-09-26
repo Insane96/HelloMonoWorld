35ea3377ce9b3b4152d04cac29773bf81dc7e961
Alberto <delvillano.alberto@gmail.com>
2022-09-26 16:59:21 +0200

    Entity Knockback resistance now actually works as a resistance Added hit time to show the entity red after hit Better health bar position

--- Entity.cs
+++ Entity.cs
@@ -12,15 +12,15 @@ namespace HelloMonoWorld;
 
 public class Entity : GameObject
 {
-    public float movementSpeed = 100f;
-    public Vector2 deltaMovement = Vector2.Zero;
+    public float MovementSpeed { get; set; } = 100f;
+    public Vector2 DeltaMovement { get; set; } = Vector2.Zero;
 
     private float maxHealth;
 
     public float MaxHealth
     {
         get { return maxHealth; }
-        //If higher max health than before then heal by the difference, otherwise prevent health from begin higher than MaxHealth
+        //If higher max health than before then heal by the difference, otherwise prevent health from being higher than MaxHealth
         set
         {
             float diff = value - this.maxHealth;
@@ -33,11 +33,11 @@ public class Entity : GameObject
     }
 
     public float Health { get; set; } = 0f;
-    public double immunityTime = 0d;
-    public bool knockbacked = false;
-    public double knockbackResistance = 0.9d;
+    public double HitTime { get; set; } = 0d;
+    public bool Knockbacked { get; set; } = false;
+    public double KnockbackResistance { get; set; } = 0.1d;
 
-    public Rectangle Bounds = Rectangle.Empty;
+    public Rectangle Bounds { get; set; } = Rectangle.Empty;
 
     /// <summary>
     /// If true, Bounds will be updated each cycle
@@ -52,15 +52,27 @@ public class Entity : GameObject
     public Vector2 LeftHand { get; private set; } = new(40, 54);
     public Vector2 RightHand { get; private set; } = new(4, 54);
 
+    private Color originalColor;
+
+    public Color OriginalColor
+    {
+        get { return originalColor; }
+        set
+        {
+            originalColor = value;
+            this.Color = value;
+        }
+    }
+
     public static Texture2D healthBarTexture = new(Graphics.graphics.GraphicsDevice, 1, 1);
 
     public Entity(string id) : this(id, id)
     {
+
     }
 
     public Entity(string id, string spriteName) : base(id, spriteName)
     {
-        //this.UpdateBounds();
         healthBarTexture.SetData(new[] { Color.White });
     }
 
@@ -69,24 +81,28 @@ public class Entity : GameObject
         this.Move();
         if (this.ShouldUpdateBounds)
             this.UpdateBounds();
-        if (this.immunityTime > 0)
-            this.immunityTime -= Time.DeltaTime;
+        if (this.HitTime > 0)
+        {
+            this.HitTime -= Time.DeltaTime;
+            if (this.HitTime <= 0d)
+                this.Color = this.OriginalColor;
+        }
     }
 
     public void Move()
     {
-        if (this.deltaMovement != Vector2.Zero)
+        if (this.DeltaMovement != Vector2.Zero)
         {
-            this.position += Vector2.Multiply(this.deltaMovement, (float)Time.DeltaTime);
+            this.Position += Vector2.Multiply(this.DeltaMovement, (float)Time.DeltaTime);
 
-            if (!this.knockbacked)
-                this.deltaMovement = Vector2.Zero;
+            if (!this.Knockbacked)
+                this.DeltaMovement = Vector2.Zero;
             else
             {
-                this.deltaMovement = Vector2.Multiply(this.deltaMovement, (float)this.knockbackResistance);
-                if (this.deltaMovement.Length() < 5f)
+                this.DeltaMovement = Vector2.Multiply(this.DeltaMovement, (float)(1f - this.KnockbackResistance));
+                if (this.DeltaMovement.Length() < 5f)
                 {
-                    this.knockbacked = false;
+                    this.Knockbacked = false;
                 }
             }
         }
@@ -96,23 +112,25 @@ public class Entity : GameObject
     {
         if (force == 0f)
             return;
-        this.knockbacked = true;
-        this.deltaMovement = Vector2.Multiply(direction, force);
+        this.Knockbacked = true;
+        this.DeltaMovement = Vector2.Multiply(direction, force);
     }
 
     public void UpdateBounds()
     {
-        this.Bounds = new Rectangle((int)(this.position.X - (this.texture.Width * this.origin.X)), (int)(this.position.Y - (this.texture.Height * this.origin.Y)), this.texture.Width, this.texture.Height);
+        this.Bounds = new Rectangle((int)(this.Position.X - (this.Texture.Width * this.Origin.X)), (int)(this.Position.Y - (this.Texture.Height * this.Origin.Y)), this.Texture.Width, this.Texture.Height);
     }
 
     public virtual bool Hurt(float damage, float knockback = 0f)
     {
-        if (this.IsImmune()
-            || this.IsDead())
+        if (this.IsDead())
             return false;
 
         this.Health -= damage;
-        //this.immunityTime = immunityTime;
+        
+        this.HitTime = 0.1d;
+        this.Color = Color.Red;
+
         if (this.IsDead())
         {
             this.OnDeath();
@@ -134,11 +152,6 @@ public class Entity : GameObject
     public virtual void OnDeath()
     {
         this.MarkForRemoval();
-    }
-
-    public virtual bool IsImmune()
-    {
-        return this.immunityTime > 0d;
     }
 
     public virtual bool IsDead()
@@ -182,7 +195,7 @@ public class Entity : GameObject
         float length = input.LengthSquared();
         if (length < 1e-4)
             return Vector2.Zero;
-        return Vector2.Multiply(length > 1.0d ? Vector2.Normalize(input) : input, this.movementSpeed);
+        return Vector2.Multiply(length > 1.0d ? Vector2.Normalize(input) : input, this.MovementSpeed);
     }
 
     public override void Draw(SpriteBatch spriteBatch)
@@ -196,7 +209,7 @@ public class Entity : GameObject
         }
         if (this.ShouldDrawHealth)
         {
-            spriteBatch.Draw(healthBarTexture, this.position.Sum(-25, this.Bounds.Height / 2 + 25), null, Color.FromNonPremultiplied(255, 100, 100, 192), 0f, Origins.CenterLeft, new Vector2((float)(this.Health / this.MaxHealth) * 50, 10), SpriteEffects.None, 0f);
+            spriteBatch.Draw(healthBarTexture, this.Position.Sum(-25, this.Bounds.Height / 2 + 10), null, Color.FromNonPremultiplied(255, 100, 100, 192), 0f, Origins.CenterLeft, new Vector2((float)(this.Health / this.MaxHealth) * 50, 8), SpriteEffects.None, 0f);
         }
     }
 }
