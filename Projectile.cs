d49cddf3decfa371f828f03f14b8d6688f715d0e
Alberto <delvillano.alberto@gmail.com>
2022-09-27 13:08:41 +0200

    cleanup

--- Projectile.cs
+++ Projectile.cs
@@ -1,40 +1,35 @@
 ﻿using HelloMonoWorld.Engine;
 using Microsoft.Xna.Framework;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HelloMonoWorld
+namespace HelloMonoWorld;
+
+public class Projectile : Entity
 {
-    public class Projectile : Entity
+    public Vector2 direction;
+    public float damage;
+    public float knockback;
+    public Entity owner;
+
+    public Projectile(string id, string spriteName, Vector2 direction, float damage, float knockback, Entity owner) : base(id, spriteName)
     {
-        public Vector2 direction;
-        public float damage;
-        public float knockback;
-        public Entity owner;
+        this.direction = direction;
+        this.damage = damage;
+        this.knockback = knockback;
+        this.owner = owner;
+    }
 
-        public Projectile(string id, string spriteName, Vector2 direction, float damage, float knockback, Entity owner) : base(id, spriteName)
-        {
-            this.direction = direction;
-            this.damage = damage;
-            this.knockback = knockback;
-            this.owner = owner;
-        }
-
-        public override void Update()
-        {
-            this.DeltaMovement = this.direction.Multiply(this.MovementSpeed);
-            List<Entity> entitiesCollided = this.GetCollisions(typeof(Player), typeof(Hero));
-            bool hasCollided = false;
-            entitiesCollided.ForEach(entity => {
-                entity.Hurt(this.damage, this.knockback);
-                hasCollided = true;
-            });
-            if (hasCollided)
-                this.Discard();
-            base.Update();
-        }
+    public override void Update()
+    {
+        this.DeltaMovement = this.direction.Multiply(this.MovementSpeed);
+        List<Entity> entitiesCollided = this.GetCollisions(typeof(Player), typeof(Hero));
+        bool hasCollided = false;
+        entitiesCollided.ForEach(entity => {
+            entity.Hurt(this.damage, this.knockback);
+            hasCollided = true;
+        });
+        if (hasCollided)
+            this.Discard();
+        base.Update();
     }
 }
