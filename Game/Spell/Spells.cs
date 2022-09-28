d0c9a6ee63c150fac25561f4ece5bca417d50540
Alberto <delvillano.alberto@gmail.com>
2022-09-28 15:53:11 +0200

    Revert weapon

---
+++ Game/Spell/Spells.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloMonoWorld.Game.Spell
+{
+    public static class Spells
+    {
+        public static SpellDefinition MagicBullet = new("magic_bullet", 1f, 50f, 250f, 1f);
+        public static SpellDefinition EnergyBall = new("energy_ball", 1f, 50f, 250f, 1f);
+    }
+}
