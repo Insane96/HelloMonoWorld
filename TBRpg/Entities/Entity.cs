7a9e4271362dd124066bceb4d7be1a110e2fc695
Insane96 <delvillano.alberto@gmail.com>
2024-11-08 22:41:34 +0100

    Removed project

--- TBRpg/Entities/Entity.cs
+++
@@ -1,9 +0,0 @@
-﻿using Engine;
-
-namespace TBRpg.Entities;
-
-public class Entity : GameObject
-{
-    public int MaxHealth { get; set; }
-    public int Health { get; set; }
-}
--- TBRpg/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿using var game = new TBRpg.RpgGame();
-game.Run();
