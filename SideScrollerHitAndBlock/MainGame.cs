03fa87e4c594f7fbab2e0fc8830fdcf17398d1eb
Insane96 <delvillano.alberto@gmail.com>
2025-05-25 11:50:12 +0200

    Side scroller project

---
+++ SideScrollerHitAndBlock/MainGame.cs
@@ -0,0 +1,46 @@
+﻿using Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SideScrollerHitAndBlock;
+
+public class MainGame : EngineGame
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public MainGame(int width, int height) : base(width, height)
+    {
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
---
+++ SideScrollerHitAndBlock/Program.cs
@@ -0,0 +1,4 @@
+﻿using SideScrollerHitAndBlock;
+
+using var game = new MainGame(720, 1280);
+game.Run();
